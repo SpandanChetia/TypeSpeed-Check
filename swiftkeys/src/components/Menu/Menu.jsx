b4d2d13fcd2847b7e3278a1c5f81fd2df71c74ba
SpandanPriyam22 <spandanpriyam1976@gmail.com>
2024-06-18 17:16:35 +0530

    Files added

--- swiftkeys/src/components/Menu/Menu.jsx
+++ swiftkeys/src/components/Menu/Menu.jsx
@@ -1,29 +1,56 @@
-import React from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './Menu.css';
+import React, { useState } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./Menu.css";
 
 const items = [
-  { type: 'time', label: 'Time' },
-  { type: 'words', label: 'Words' },
-  { type: 'quote', label: 'Quotes' }
+  { type: "time", label: "time" },
+  { type: "words", label: "words" },
+  { type: "quote", label: "quotes" },
 ];
 
 const iconMap = {
-  time: 'fa-clock',
-  words: 'a',
-  quote: 'fa-quote-left'
+  time: "fa-clock",
+  words: "S",
+  quote: "fa-quote-left",
+};
+
+const quantity_amount = {
+  time: ["15", "30", "60"],
+  words: ["10", "25", "50"],
+  quote: ["all", "short", "medium", "long"],
 };
 
 export default function Menu() {
+  const [activeQuantity, changeQuantity] = useState("time");
+
+  const quantity_amount_items = quantity_amount[activeQuantity];
+  const handle_quantitytype = (newtype) => {
+    changeQuantity(newtype);
+  };
   return (
     <div className="menu">
       <ol className="quantity-type">
         {items.map((item) => (
-          <li key={item.type} className="quantity-type-options">
-            {item.type==='words' ? 
-            (<span className="letter-icon">{iconMap[item.type]}</span>) :
-            (<i className={`fas ${iconMap[item.type]}`} />)}
+          <li
+            key={item.type}
+            className="quantity-type-options"
+            onClick={() => {
+              handle_quantitytype(item.type);
+            }}
+          >
+            {item.type === "words" ? (
+              <span className="letter-icon">{iconMap[item.type]}</span>
+            ) : (
+              <i className={`fas ${iconMap[item.type]}`} />
+            )}
             {item.label}
+          </li>
+        ))}
+      </ol>
+      <ol className="quantity-amount">
+        {quantity_amount_items.map((amount, index) => (
+          <li key={index} className="amount-value">
+            {amount}
           </li>
         ))}
       </ol>
